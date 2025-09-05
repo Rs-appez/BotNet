7d1fc8fff30cd442f8cd33881dd9a111402c57b0
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-09-05 23:04:21 +0200

    add channel_calendar

--- config.py
+++ config.py
@@ -9,6 +9,9 @@ GOOGLE_API_KEY = config("GOOGLE_API_KEY")
 
 # channels
 CHANNELBOT_LOG_ID = 1110930833314938950
+TECHNOFUTUR_CALENDAR_CHANNEL_ID = config(
+    "TECHNOFUTUR_CALENDAR_CHANNEL_ID", default=None
+)
 
 # link
 URL_SHEET_API = "https://sheets.googleapis.com/v4/spreadsheets/"
