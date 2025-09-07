5180c975f0be8ca8057a494594b7f5497eb0638a
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-09-07 02:42:58 +0200

    feat(scheduler): add calendar update notification job
    - Add __check_update_calendar job to notify channel when schedule updates
    - Refactor channel fetching to async method with caching

--- models/scheduler.py
+++ models/scheduler.py
@@ -1,5 +1,6 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
+from nextcord import TextChannel
 
 import datetime
 import config
@@ -10,8 +11,9 @@ from bot.botNet import BotNet
 
 class Scheduler:
     def __init__(self, bot: BotNet, calendar: Calendar) -> None:
-        self.calendar = calendar
-        self.bot = bot
+        self.calendar: Calendar = calendar
+        self.bot: BotNet = bot
+        self.announce_channel: TextChannel = None
         self.scheduler = AsyncIOScheduler()
         self.__init_schedule()
         self.__add_jobs()
@@ -29,6 +31,18 @@ class Scheduler:
             self.__get_week_calendar,
             CronTrigger(hour=16, minute=30, second=00, day_of_week="thu"),
         )
+        self.scheduler.add_job(
+            self.__check_update_calendar,
+            CronTrigger(hour=17, minute=00, second=00),
+        )
+
+    async def __get_calendar_channel(self) -> TextChannel:
+        if not self.announce_channel:
+            channel = await self.bot.fetch_channel(
+                config.TECHNOFUTUR_CALENDAR_CHANNEL_ID
+            )
+            self.announce_channel = channel
+        return self.announce_channel
 
     async def __get_week_calendar(self):
         next_today = datetime.datetime.now() + datetime.timedelta(days=7)
@@ -36,8 +50,19 @@ class Scheduler:
         month = next_today.month
         next_week = self.calendar.get_week(day, month)
 
-        announce_channel = self.bot.get_channel(config.TECHNOFUTUR_CALENDAR_CHANNEL_ID)
+        self.calendar.next_week = next_week
 
-        if next_week and announce_channel:
-            msg = "Here is the schedule for next week:\n```" + str(next_week) + "```"
-            await announce_channel.send(msg)
+        if next_week:
+            msg = "Here is the schedule for next week:\n```" + \
+                str(next_week) + "```"
+            channel = await self.__get_calendar_channel()
+            await channel.send(msg)
+
+    async def __check_update_calendar(self):
+        if week := self.calendar.is_updated():
+            self.calendar.next_week = week
+
+            msg = "@here\nThe schedule has been updated !!! :\n```" + \
+                str(week) + "```"
+            channel = await self.__get_calendar_channel()
+            await channel.send(msg)
